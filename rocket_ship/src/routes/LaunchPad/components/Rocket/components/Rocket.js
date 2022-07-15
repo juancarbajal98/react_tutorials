34b9555969fdcf7df09c59ac53614452e1c2f70b
juancarbajal98 <juancarbajal98@gmail.com>
2022-07-15 11:46:41 -0700

    rocket ship functional component solution

--- rocket_ship/src/routes/LaunchPad/components/Rocket/components/Rocket.js
+++ rocket_ship/src/routes/LaunchPad/components/Rocket/components/Rocket.js
@@ -1,11 +1,29 @@
-import React, { useState, Component } from 'react';
+import React, { useState, Component, useEffect, memo } from 'react';
 import RocketCore from './RocketCore';
 
+/* 
+INITIAL FUNCTIONAL COMPONENT - missing memo wrapper
 export function FunctionalRocket() {
   const [initialLaunchTime] = useState(Date.now());
 
   return <RocketCore initialLaunchTime={initialLaunchTime} />;
 }
+*/
+
+/* 
+SOLUTIUON - memo wrapper
+from docs: If your component renders the same result given the same props, 
+you can wrap it in a call to React.memo for a performance boost in some cases 
+by memoizing the result. This means that React will skip rendering the component, 
+and reuse the last rendered result.
+
+
+*/
+export const FunctionalRocket = memo(() => {
+  const [initialLaunchTime] = useState(Date.now());
+
+  return <RocketCore initialLaunchTime={initialLaunchTime} />;
+})
 
 export class ClassRocket extends Component {
   constructor(props) {
